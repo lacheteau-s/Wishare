f494e9edf110c311dc269bd341febc04bb4ec516
Sulli <lacheteau.s@gmail.com>
2021-04-19 11:58:19 +0200

    Basic Header component

---
+++ ui/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+	<v-app-bar
+		app
+		color="primary">
+		Wishare
+
+		<v-spacer />
+
+		<v-btn 
+			rounded
+			text
+			class="mx-1"
+			@click="onClick">Sign in</v-btn>
+		<v-btn
+			rounded
+			class="mx-1"
+			@click="onClick">Sign up</v-btn>
+	</v-app-bar>
+</template>
+
+<script lang="ts">
+
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Header extends Vue {
+	onClick() {
+		console.log("test");
+	}
+}
+
+</script>
