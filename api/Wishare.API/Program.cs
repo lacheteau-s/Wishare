514bd48f86e4e202b902890acb8992c96753f7a8
Sulli <lacheteau.s@gmail.com>
2021-04-23 16:17:39 +0200

    UseKestrel + remove IIS config from launchSettings

--- api/Wishare.API/Program.cs
+++ api/Wishare.API/Program.cs
@@ -20,7 +20,7 @@ namespace Wishare.API
 			Host.CreateDefaultBuilder(args)
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
-					webBuilder.UseStartup<Startup>();
+					webBuilder.UseStartup<Startup>().UseKestrel();
 				});
 	}
 }
